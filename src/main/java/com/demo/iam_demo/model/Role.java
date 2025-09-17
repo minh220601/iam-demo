ef4a749e840604486b3c1a63c4b8787be1e1c17d
minh2262001 <tranminh2262001@gmail.com>
2025-09-17 11:54:30 +0700

    init commit

---
+++ src/main/java/com/demo/iam_demo/model/Role.java
@@ -0,0 +1,16 @@
+package com.demo.iam_demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, name = "name")
+    private ERole name;
+}
