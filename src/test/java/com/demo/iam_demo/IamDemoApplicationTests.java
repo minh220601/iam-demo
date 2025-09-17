1649679421d6db472d7d14b013ff6c924f969ddb
minh220601 <tranminh2262001@gmail.com>
2025-09-17 10:36:37 +0700

    init commit

---
+++ src/test/java/com/demo/iam_demo/IamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.iam_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
