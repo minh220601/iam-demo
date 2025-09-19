9c39d08bd2fe2a59fdb2c3d4d5676333027aca9d
minh2262001 <tranminh2262001@gmail.com>
2025-09-19 16:05:49 +0700

    add role entity

--- src/main/java/com/demo/iam_demo/entity/Role.java
+++ src/main/java/com/demo/iam_demo/entity/Role.java
@@ -1,26 +1,23 @@
 package com.demo.iam_demo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Enumerated(EnumType.STRING)
-    @Column(length = 20)
-    private ERole name;
-
-    public Role(){}
-
-    public Role(ERole name){
-        this.name = name;
-    }
-
-    public Integer getId(){return id;}
-    public void setId(Integer id){this.id = id;}
-    public ERole getName(){return name;}
-    public void setName(ERole name){this.name = name;}
+    @Column(nullable = false,unique = true, length = 20)
+    private String name;
 }
