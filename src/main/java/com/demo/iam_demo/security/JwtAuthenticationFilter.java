9986488c8ce8efdf69eef846a6a69df2cecf4e2c
minh2262001 <tranminh2262001@gmail.com>
2025-09-19 16:03:52 +0700

    add dependencies

---
+++ src/main/java/com/demo/iam_demo/security/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.demo.iam_demo.security;
+
+import com.demo.iam_demo.service.UserDetailsServiceImpl;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFiler extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+        throws ServletException, IOException{
+
+        //lấy Authorization header
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
+            String token = authHeader.substring(7);
+
+            if(jwtUtils.validateToken(token)){
+                //lấy subject (email/username) từ token
+                String username = jwtUtils.extractSubject(token);
+
+                //kiểm tra Security context chưa có user
+                if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                    //nếu hợp lệ -> set authentication
+                    UsernamePasswordAuthenticationToken authToken =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails,
+                                    null,
+                                    userDetails.getAuthorities()
+                            );
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+        }
+
+        //cho request đi tiếp
+        filterChain.doFilter(request, response);
+    }
+}
