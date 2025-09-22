c59f12f3e7596b26ef0573491b4951b3a172857a
minh2262001 <tranminh2262001@gmail.com>
2025-09-22 15:53:46 +0700

    add user repository

--- src/main/java/com/demo/iam_demo/repository/UserRepository.java
+++ src/main/java/com/demo/iam_demo/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.demo.iam_demo.repository;
 
-import com.demo.iam_demo.entity.User;
+import com.demo.iam_demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-    Boolean existsByUsername(String username);
-    Boolean existsByEmail(String email);
+    Optional<User> findByEmail(String Email);
+    boolean existsByEmail(String email);
 }
