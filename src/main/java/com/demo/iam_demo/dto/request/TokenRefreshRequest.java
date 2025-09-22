d18dd7872e82a497da77455d45bf71a907f3d529
minh2262001 <tranminh2262001@gmail.com>
2025-09-22 16:01:54 +0700

    add request and response for token refresh method

--- src/main/java/com/demo/iam_demo/dto/request/TokenRefreshRequest.java
+++ src/main/java/com/demo/iam_demo/dto/request/TokenRefreshRequest.java
@@ -1,4 +1,10 @@
 package com.demo.iam_demo.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class TokenRefreshRequest {
+    @NotBlank
+    private String refreshToken;
 }
--- src/main/java/com/demo/iam_demo/dto/response/TokenRefreshResponse.java
+++ src/main/java/com/demo/iam_demo/dto/response/TokenRefreshResponse.java
@@ -1,4 +1,11 @@
 package com.demo.iam_demo.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class TokenRefreshResponse {
+    private String accessToken;
+    private String refreshToken;
 }
