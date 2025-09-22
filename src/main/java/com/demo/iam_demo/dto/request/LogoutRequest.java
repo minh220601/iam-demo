e013984f5008847a4bb6b5d8f8828625816cff0e
minh2262001 <tranminh2262001@gmail.com>
2025-09-22 13:51:31 +0700

    add request for logout

--- src/main/java/com/demo/iam_demo/dto/request/LogoutRequest.java
+++ src/main/java/com/demo/iam_demo/dto/request/LogoutRequest.java
@@ -1,4 +1,12 @@
 package com.demo.iam_demo.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LogoutRequest {
+    @NotBlank
+    @Email
+    private String email;
 }
