b32b4fb8287dfd5f9533b5fff909d966b9f8618a
minh2262001 <tranminh2262001@gmail.com>
2025-09-22 16:32:28 +0700

    add get all users method

--- src/main/java/com/demo/iam_demo/service/UserService.java
+++ src/main/java/com/demo/iam_demo/service/UserService.java
@@ -1,4 +1,21 @@
 package com.demo.iam_demo.service;
 
+import com.demo.iam_demo.model.User;
+import com.demo.iam_demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    // lấy danh sách tất cả user
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
 }
