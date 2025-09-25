609becc0c5031539d0d02b1a9e18ff9cfb580e5a
minh2262001 <tranminh2262001@gmail.com>
2025-09-25 11:02:40 +0700

    add request to update user profile

--- src/main/java/com/demo/iam_demo/dto/request/UpdateProfileRequest.java
+++ src/main/java/com/demo/iam_demo/dto/request/UpdateProfileRequest.java
@@ -1,4 +1,15 @@
 package com.demo.iam_demo.dto.request;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class UpdateProfileRequest {
+    // dùng cho user chỉnh sửa info
+    private String username;
+    private LocalDate birthDate;
+    private String address;
+    private String phone;
+    private String avatar;
 }
