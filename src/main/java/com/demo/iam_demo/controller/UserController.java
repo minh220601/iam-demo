6f00a89be2360c523176913424d0d0f5765b6ffd
minh2262001 <tranminh2262001@gmail.com>
2025-09-26 16:22:40 +0700

    add api update info for user logged in and api update user info (admin only)

--- src/main/java/com/demo/iam_demo/controller/UserController.java
+++ src/main/java/com/demo/iam_demo/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.demo.iam_demo.controller;
 
+import com.demo.iam_demo.dto.request.UpdateProfileRequest;
+import com.demo.iam_demo.dto.request.UserInfoRequest;
+import com.demo.iam_demo.dto.response.UserInfoResponseDTO;
 import com.demo.iam_demo.model.User;
 import com.demo.iam_demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,22 +22,36 @@ public class UserController {
     // lấy danh sách user (admin)
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<List<User>> getAllUsers(){
+    public ResponseEntity<List<UserInfoResponseDTO>> getAllUsers(){
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    // xem info user
+    // xem info 1 user theo id (admin)
     @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ADMIN','USER')")
-    public ResponseEntity<User> getUserById(@PathVariable Long id){
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserInfoResponseDTO> getUserById(@PathVariable Long id){
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
-    // update info (user hoặc admin)
-    @PutMapping("/{id}")
+    // xem info của user đang đăng nhập
+    @GetMapping("/me")
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
-    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user){
-        return ResponseEntity.ok(userService.updateUser(id, user));
+    public ResponseEntity<UserInfoResponseDTO> getProfile(Authentication authentication) {
+        return ResponseEntity.ok(userService.getUserByEmail(authentication.getName()));
+    }
+
+    // update info của user đang đăng nhập
+    @PutMapping("/me")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    public ResponseEntity<UserInfoResponseDTO> updateUserProfile(Authentication authentication, @RequestBody UpdateProfileRequest request){
+        return ResponseEntity.ok(userService.updateUserByEmail(authentication.getName(), request));
+    }
+
+    // update info user khác (admin)
+    @PutMapping("/admin/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserInfoResponseDTO> updateUserByAdmin(@PathVariable Long id, @RequestBody UserInfoRequest request){
+        return ResponseEntity.ok(userService.updateUserByAdmin(id, request));
     }
 
     // xóa user (admin)
