f17697aa69361ca0c4f80dae58ee1c07e139b8d8
minh2262001 <tranminh2262001@gmail.com>
2025-10-14 16:28:31 +0700

    add reset password request

--- src/main/java/com/demo/iam_demo/dto/request/ResetPasswordRequest.java
+++ src/main/java/com/demo/iam_demo/dto/request/ResetPasswordRequest.java
@@ -1,4 +1,16 @@
 package com.demo.iam_demo.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class ResetPasswordRequest {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String otp;
+
+    @NotBlank
+    private String newPassword;
 }
