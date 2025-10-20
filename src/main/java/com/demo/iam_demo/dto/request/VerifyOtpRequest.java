d3e1e7863b40dc349e971397ac85f4808cfc7867
minh2262001 <tranminh2262001@gmail.com>
2025-10-20 10:55:36 +0700

    add verifi otp request

---
+++ src/main/java/com/demo/iam_demo/dto/request/VerifyOtpRequest.java
@@ -0,0 +1,19 @@
+package com.demo.iam_demo.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VerifyOtpRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String otp;
+}
