a97f6179889443a337ec5fc20b0c6a1f45eb1ad6
minh2262001 <tranminh2262001@gmail.com>
2025-10-22 17:03:40 +0700

    add UserActivityLog service

--- src/main/java/com/demo/iam_demo/service/UserActivityLogService.java
+++ src/main/java/com/demo/iam_demo/service/UserActivityLogService.java
@@ -1,4 +1,23 @@
 package com.demo.iam_demo.service;
 
+import com.demo.iam_demo.model.UserActivityLog;
+import com.demo.iam_demo.repository.UserActivityLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class UserActivityLogService {
+    private final UserActivityLogRepository logRepository;
+
+    public void log(Long userId, String activity){
+        UserActivityLog log = UserActivityLog.builder()
+                .userId(userId)
+                .activity(activity)
+                .createdAt(LocalDateTime.now())
+                .build();
+        logRepository.save(log);
+    }
 }
