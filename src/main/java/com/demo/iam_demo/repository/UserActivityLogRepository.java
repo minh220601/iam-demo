5835e1a63c927704a47d0bac74c13017678485e1
minh2262001 <tranminh2262001@gmail.com>
2025-10-22 16:59:07 +0700

    add UserActivityLog repository

--- src/main/java/com/demo/iam_demo/repository/UserActivityLogRepository.java
+++ src/main/java/com/demo/iam_demo/repository/UserActivityLogRepository.java
@@ -1,4 +1,9 @@
 package com.demo.iam_demo.repository;
 
-public interface UserActivityLogRepository {
+import com.demo.iam_demo.model.UserActivityLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserActivityLogRepository extends JpaRepository<UserActivityLog, Long> {
 }
