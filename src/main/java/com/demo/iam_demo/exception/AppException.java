c400bdc333bc93d1d4f47ffe7bf136df85153f2f
minh2262001 <tranminh2262001@gmail.com>
2025-10-27 16:01:00 +0700

    add AppException file

--- src/main/java/com/demo/iam_demo/exception/AppException.java
+++ src/main/java/com/demo/iam_demo/exception/AppException.java
@@ -3,10 +3,16 @@ package com.demo.iam_demo.exception;
 import lombok.Getter;
 
 @Getter
-public class AuthException extends RuntimeException{
-    private ErrorCode errorCode;
-    public AuthException(ErrorCode errorCode){
+public class AppException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public AppException(ErrorCode errorCode){
         super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public AppException(ErrorCode errorCode, String customMessage){
+        super(customMessage);
         this.errorCode = errorCode;
     }
 }
