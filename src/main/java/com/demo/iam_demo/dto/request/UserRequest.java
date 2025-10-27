c3382cf92aeaa10bc401cd53db8b6288b5889c29
minh2262001 <tranminh2262001@gmail.com>
2025-10-27 15:49:54 +0700

    add user request

--- src/main/java/com/demo/iam_demo/dto/request/UserRequest.java
+++ src/main/java/com/demo/iam_demo/dto/request/UserRequest.java
@@ -1,4 +1,19 @@
 package com.demo.iam_demo.dto.request;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
 public class UserRequest {
+    private String username;
+    private String email;
+    private String password;
+    private LocalDate birthDate;
+    private String address;
+    private String phone;
+    private String avatar;
+    private boolean active;
+    private Set<String> roles;
 }
